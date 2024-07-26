ee0c16a7a23570e1918442c10d2483291357b54b
Masha <masha@MacBook-Air.local>
2024-07-26 18:18:58 +0200

    styles for progress-bar and math floor

--- src/components/ProgressBar.tsx
+++ src/components/ProgressBar.tsx
@@ -1,15 +1,17 @@
+import '../styles/ProgressBar.css';
+
 type ProgressBarProps = {
   current: number;
   total: number;
 }
 
 function ProgressBar(props: ProgressBarProps) {
-  const percentage = (props.current / props.total) * 100;
+  const percentage = Math.round((props.current / props.total) * 100);
 
   return (
     <div className="progress-bar">
-      <div className="progress-bar__inner" style={{ width: `${percentage}%` }}></div>
       <div className="progress-bar__text">{`Question ${props.current} out of ${props.total}`}</div>
+      <div className="progress-bar__inner" style={{ width: `${percentage}%` }}></div>
     </div>
   );
 }
